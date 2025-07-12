be71cfcfb4d2e551a25682f41274e8cc04dd4def
Kashaf-u-Doja <kashafmirza56@gmail.com>
2025-07-13 02:03:51 +0500

    create a page in next js

---
+++ src/WebPages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import BannerSection from "@/PageSections/Banner/BannerSection";
+import DetailSection from "@/PageSections/Details/DetailSection";
+import Footer from "@/PageSections/Footer/Footer";
+import React from "react";
+
+function LandingPage() {
+  return (
+    <>
+      <BannerSection />
+      <DetailSection />
+      <Footer />
+    </>
+  );
+}
+
+export default LandingPage;
---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import LandingPage from "@/WebPages/LandingPage";
+
+export default function Home() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}
